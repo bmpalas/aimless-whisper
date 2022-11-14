bd7dcb68976288354a5c2a4b296045f1b3471c95
funfetus <funfetus@gmail.com>
2022-11-14 14:28:27 -0800

    create first prototype

--- aimless-whisper.js
+++ aimless-whisper.js
@@ -1,8 +1,27 @@
-let hero = "Kesh Marheim"
+let protagx = "Kesh"
 
-let villain = ["The Evil Corporation", "The Shadowy Cabal", "The Traitorous Commander"]
+console.log(`${protag} is the protagonist of Aimless Whisper`);
 
 
+const orgs = [ "Legion", "Circle", "Commanders", "Raiders"];
 
-console.log(`#{hero} vs. the #{villain}`);
+const themes = [ "Wolves", "The Hawk", "The Claw", "The Chain", "Shackles"];
 
+const adj = [ "Blue", "Black", "Red", "Crimson", "Velvet", "Iron"];
+
+const noun = [ "Wolf", "Hound", "Saint", "Priest", "King", "Queen", "Prince", "Boss", "Hand", "Blade", "Flame"];
+
+const desc = [ "fastidious", "vicious", "calculating", "beautiful", "terrifying", "psychotic", "charming", "unpredicably violent", "disarmingly meek", "brash", "confident"]
+
+let antagOrg = (orgs[Math.floor(Math.random() * orgs.length)]) + " of " + 
+    (themes[Math.floor(Math.random() * themes.length)]);
+
+
+let antagLeader = (adj[Math.floor(Math.random() * orgs.length)]) + " " + (noun[Math.floor(Math.random() * noun.length)]);
+
+
+Math.random
+
+console.log(`In this chapter, ${x} takes on the ${antagOrg}.`);
+
+console.log(`The leader of ${antagOrg} is ${antagLeader}, a ${antagDesc}.`); they are 2
